6dc1f6897463fd3f01cb8f1da609184073a7f904
Anthony Salinas Suarez <anthonysalinassuarez@Anthonys-MBP.lan>
2023-07-30 13:46:06 -0700

    More stylistic changes

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { BrowserRouter as Router, Routes, Route, Link, NavLink} from 'react-rout
 import HomePage from "./HomePage";
 import Resume from "./ResumePage";
 import Contact from "./ContactPage";
-import CS_Portfolio from "./CS-Portfolio";
+import CsPortfolio from "./CS-Portfolio";
 import AboutPage from "./AboutPage";
 
 function App() {
@@ -27,38 +27,44 @@ function App() {
       </header>
     </div>*/
     <Router>
-      <div className = "image-div inline-div">
-        <NavLink to = "/" className = "link-style" id = "home-link">
-          <img src = {LogoImage} width = "100%" height = "100%"></img> 
-        </NavLink>
-      </div>
-      <div className = "inline-div">
-        <nav>
-          <ul className = "list-style">
-            <li>
-              <NavLink to = "/" className = "link-style" id = "home-link">Home</NavLink>
-            </li>
-            <li>
-              <NavLink to = "/about" className = "link-style" id = "about-page">About</NavLink>
-            </li>
-            <li>
-              <NavLink to = "/cs_portfolio" className = "link-style">CS Portfolio</NavLink>
-            </li>
-            <li>
-              <NavLink to = "/resume" className = "link-style">Resume</NavLink>
-            </li>
-            <li>
-              <NavLink to = "/contact" className = "link-style">Contact</NavLink>
-            </li>
-          </ul>
-        </nav>
-        <Routes>
-          <Route exact path = "/" element = {<HomePage />} />
-          <Route path = "/about" element = {<AboutPage />} />
-          <Route path = "/cs_portfolio" element = {<CS_Portfolio/>} />
-          <Route path = "/resume" element = {<Resume/>}/>
-          <Route path = "/contact" component = {<Contact/>} />
-        </Routes>
+      <header className = "parent-header">
+        <div className = "image-div inline-div">
+          <NavLink to = "/" className = "link-style" id = "home-link">
+            <img src = {LogoImage} width = "100%" height = "100%"></img> 
+          </NavLink>
+        </div>
+        <div className = "list-div inline-div">
+          <nav>
+            <ul className = "list-style">
+              <li>
+                <NavLink to = "/" className = "link-style" id = "home-link">Home</NavLink>
+              </li>
+              <li>
+                <NavLink to = "/about" className = "link-style" id = "about-page">About</NavLink>
+              </li>
+              <li>
+                <NavLink to = "/cs_portfolio" className = "link-style">CS Portfolio</NavLink>
+              </li>
+              <li>
+                <NavLink to = "/resume" className = "link-style">Resume</NavLink>
+              </li>
+              <li>
+                <NavLink to = "/contact" className = "link-style">Contact</NavLink>
+              </li>
+            </ul>
+          </nav>
+          <Routes>
+            <Route exact path = "/" element = {<HomePage />} />
+            <Route path = "/about" element = {<AboutPage />} />
+            <Route path = "/cs_portfolio" element = {<CsPortfolio/>} />
+            <Route path = "/resume" element = {<Resume/>}/>
+            <Route path = "/contact" component = {<Contact/>} />
+          </Routes>
+        </div>
+      </header>
+
+      <div className = "header-content-separator">
+        <hr className = "separator"></hr>
       </div>
     </Router>
   );
