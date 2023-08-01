fe2ea82a3d8b98b1ea4d2f44ca69a02615b9f6b2
Anthony Salinas Suarez <anthonysalinassuarez@Anthonys-MBP.lan>
2023-07-31 20:06:50 -0700

    Just about done with my home page

--- src/HomePage.js
+++ src/HomePage.js
@@ -5,7 +5,7 @@ import BabyTaylor from "./baby-taylor.jpeg"
 
 function HomePage() {
     return (
-        <div>
+        <div className = "main-home-page">
             <div className = "introduction">
                 <h1 id = "welcome-h1">
                     Welcome to my Website!
@@ -13,18 +13,18 @@ function HomePage() {
                 <h2>
                     I am glad to have you here! I made this website with the hopes of sharing a bit about myself and to
                     showcase the different skills and experiences I have accumulated as an Electrical Engineering & Computer Sciences (EECS) student at the University of California, Berkeley.
-                    Feel free to take a look around and explore to learn more about me and stay tuned for future updates to this website!
+                    Feel free to look around and explore to learn more about me and stay tuned for future updates to this website!
                 </h2>
             </div>
             <div className = "polaroid-images">
                 <div className = "double-stacked">
                     <div className = "polaroid-container">
                         <img src = {MachoPhoto} alt = "My dog: Macho"/>
-                        <div className = "polaroid-caption"> My dog: Macho </div>
+                        <div className = "polaroid-caption"> Doggo 1: Macho</div>
                     </div>
                     <div className = "polaroid-container">
                         <img src = {LunaPhoto} alt = "My dog: Luna"/>
-                        <div className = "polaroid-caption"> My dog: Luna </div>
+                        <div className = "polaroid-caption"> Doggo 2: Luna </div>
                     </div>
                 </div>
                 <div className = "double-stacked">
@@ -37,6 +37,10 @@ function HomePage() {
                         <div className = "polaroid-caption"> My baby taylor guitar!!!</div>
                     </div>
                 </div>
+            </div>
+            <div className = "spotify-playlist">
+                <h2>If you'd like....check out my spotify playlist below!</h2>
+                <iframe src="https://open.spotify.com/embed/playlist/3gkTnIG2dHkhcBCnxgcDc1?utm_source=generator"></iframe>
             </div>
         </div>
     );
