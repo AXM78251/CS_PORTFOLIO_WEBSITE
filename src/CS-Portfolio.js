745f4cdc1983cad8544e65cac05ef346e8d2304e
Anthony Salinas Suarez <anthonysalinassuarez@Anthonys-MBP.lan>
2023-08-14 01:39:06 -0700

    Added card layout to relevant coursework section of portfolio

--- src/CS-Portfolio.js
+++ src/CS-Portfolio.js
@@ -8,6 +8,37 @@ function CS_Portfolio() {
                 <h1 className = "portfolio-section-headers">Relevant Coursework</h1>
                 <hr/>
             </div>
+            <div className = "class-cards-grid">
+                <div className = "class-card">
+                    <div className = "class-card-title">
+                        <h2>
+                            Structure & Interpretation of Computer Programs
+                        </h2>
+                        <div className = "class-card-content">
+                            <p>
+                                CS61A
+                            </p>
+                            <h2>
+                                Skills: Python, Scheme, SQL, Git Version Control 
+                            </h2>
+                            <hr></hr>
+                            <h2>
+                                Fall 2020
+
+                            </h2>
+                            <div className = "course-link-container">
+                                <div className = "course-link">
+                                    <a href = "https://inst.eecs.berkeley.edu/~cs61a/fa20/" target = "_blank" rel = "noopener noreferrer">
+                                        <h2>
+                                            Course Site
+                                        </h2>
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
             <div className = "experience-section" id = "experience-sec">
                 <hr/>
                 <h1 className = "portfolio-section-headers">Experience</h1>
